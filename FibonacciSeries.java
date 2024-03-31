f358ee1a5655479f213578aac82dd45af98aa834
01fe22bca237 <01fe22bca237@kletech.ac.in>
2024-03-31 18:22:57 +0530

    fibonacciseries

---
+++ FibonacciSeries.java
@@ -0,0 +1,16 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+            int n=5;
+            System.out.println("Fibonacci Series up to " + n + " terms:");
+            int firstTerm = 0, secondTerm = 1;
+    
+            for (int i = 1; i <= n; ++i) {
+                System.out.print(firstTerm + " ");
+    
+                int nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
+        }
+    }
+    
